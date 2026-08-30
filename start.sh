70d2484bc56df50f59d84fc726cabfddf8126236
ala.shiban <ala.shiban@datadoghq.com>
2026-08-30 11:39:42 -0400

    Enable dd-trace debug logging to diagnose missing APM data
    Local trace-agent is healthy and Node app is serving requests, but no
    spans are reaching Datadog backend. Debug logs will show whether
    dd-trace is generating/flushing spans at all.

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ export DD_SERVICE=${DD_SERVICE:-delivery-promise-service}
 export DD_ENV=${DD_ENV:-production}
 export DD_REMOTE_CONFIGURATION_ENABLED=${DD_REMOTE_CONFIGURATION_ENABLED:-true}
 export DD_DYNAMIC_INSTRUMENTATION_ENABLED=${DD_DYNAMIC_INSTRUMENTATION_ENABLED:-true}
+export DD_TRACE_DEBUG=${DD_TRACE_DEBUG:-true}
 
 echo "Starting Datadog Agent..."
 /opt/datadog-agent/bin/agent/agent run -c /etc/datadog-agent/datadog.yaml &
